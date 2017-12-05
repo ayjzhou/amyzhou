35916d18d3cdfb65a84de4586f5112aca068d2f5
MickelPickel <Michael (Jun Ke) Huang>
2017-12-04 23:47:17 -0500

    12/04/2017 added more style, made it more resize friendly

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -5,8 +5,11 @@ import {Nav, NavItem, Button} from 'react-bootstrap';
 class MainPage extends React.Component{
     constructor(props){
         super(props);
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
         this.state = {
             navBarState: 1,
+            width: '0',
+            height: '0'
         }
     }
 
@@ -14,6 +17,29 @@ class MainPage extends React.Component{
         this.setState({navBarState: key});
     }
 
+    setImageHeight(){
+        let viewportHeight = (window).height();
+        let viewportWidth = (window).width();
+        let elem = document.getElementById("section-profile-pic");
+        elem.style.height = viewportHeight*0.45;
+        elem.style.width = viewportWidth*0.45;
+    }
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+        let elem = document.getElementById("section-profile-pic");
+        elem.style.height = window.innerWidth*0.45;
+        elem.style.width = window.innerWidth*0.45;
+        console.log(window.innerWidth*0.45);
+    }
 
     render(){
         return(
@@ -24,7 +50,7 @@ class MainPage extends React.Component{
                     </Button>
                     <div id="navBar">
                         <Nav bsStyle="pills" activeKey={this.state.navBarState} onSelect={ (key) => this.handleSelected(key)} >
-                            <NavItem eventKey={1} href="/home">About Me</NavItem>
+                            <NavItem  eventKey={1} href="/home">About Me</NavItem>
                             <NavItem eventKey={2} title="Item">Android Game</NavItem>
                             <NavItem eventKey={3} >Find Me</NavItem>
                         </Nav>
@@ -33,20 +59,22 @@ class MainPage extends React.Component{
 
                 <div className="section">
                     <div className="section-text">
-                        Wait... your name is Junk? No... It's JunKe...
+                        <h1>Welcome to JunKe Huang's Planet!</h1>
+                        Wait... your name is Junk? No... It's Jun Ke...
                     </div>
                 </div>
                 <div className="profile-wrapper">
-                    <div className="section-profile-pic">
+                    <h2 id="profile-about-me"> About Me</h2>
+                    <div id="section-profile-pic">
+
                     </div>
 
                     <div className="section-profile-right">
-                        <div className="section-profile-text">some text asdf
-                            asdas
-                            asdja
-                            asdl
-                            asdas
-                            asdasldfj;alsjdf;lasjdf;lasjdf;la</div>
+                        <div className="section-profile-text">Hey! My name is Michael.<br/>
+                            I am a second year Computer Engineering student.<br/>
+                            I made this website because I got bored.
+                            <Button id="learnMore"  bsStyle="primary">Learn More</Button>
+                        </div>
                     </div>
                 </div>
 
